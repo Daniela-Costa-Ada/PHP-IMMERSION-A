ead8fda437889a9318276a34f882cc972585b520
Daniela Costa <siedanipereira@gmail.com>
2022-12-13 11:34:02 -0300

    fix crud create, now is working

--- examples/create.php
+++ examples/create.php
@@ -2,11 +2,9 @@
 require __DIR__ . "/../vendor/autoload.php";
 use Source\Models\User;
 $user = new User();
-// $user->id = '4';
-//does not save, it may be that when creating the id, I created it in the database without being an autoincremet, it is to be verified and fixed
-//update and delete is working
-$user->first_name = "ragnar";
-$user->last_name = "rock";
-$user->genre = "M";
+$user->first_name = "aria";
+$user->last_name = "stark";
+$user->genre = "F";
 $user->save();
 //var_dump($user);
+//create is working
--- examples/update.php
+++ examples/update.php
@@ -1,10 +1,10 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
 use Source\Models\User;
-$user = (new User())->findById(3);
-$user->first_name = "Deric";
-$user->last_name = "costa";
-$user->genre = "M";
+$user = (new User())->findById(2);
+$user->first_name = "Luiz";
+$user->last_name = "XVI";
+$user->genre = "F";
 $user->save();
 //var_dump($user->data());
 //update is working
